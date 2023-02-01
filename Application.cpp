e97a419e6cec22e4758ef1cc0311a6bd4150a890
gusrlLee <gusrl3204@naver.com>
2023-02-01 14:48:19 +0900

    nothing

--- Application.cpp
+++ Application.cpp
@@ -501,6 +501,7 @@ int main( int argc, char** argv )
                 timeStamp.push_back(time);
             }
 
+            // print result time 
             float sum = 0;
             for (int i = 0; i < timeStamp.size(); i++)
             {
